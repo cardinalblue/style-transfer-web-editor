918e9d196c594fb69d555db231f6f203745f15c2
Vivi Tseng <vivi.tseng@cardinalblue.com>
2023-12-18 16:46:31 -0800

    feat: add react-query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { QueryClientProvider } from '@/components/providers/QueryClient'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -9,7 +10,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <QueryClientProvider>{children}</QueryClientProvider>
+      </body>
     </html>
   )
 }
